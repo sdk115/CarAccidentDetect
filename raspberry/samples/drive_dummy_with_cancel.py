203239594d5d9454abf3e49c4bfa9d465960a7b1
성대경 <sdk11594@gmail.com>
2017-11-20 07:42:46 -0800

    error correct

--- raspberry/samples/drive_dummy_with_cancel.py
+++ raspberry/samples/drive_dummy_with_cancel.py
@@ -145,12 +145,12 @@ while True:
         flag500 = False
         flag200 = False
         flag100 = False
-    elif !flag500 and shortest_distance <500 and shortest_distance >200 :
+    elif (not flag500) and shortest_distance <500 and shortest_distance >200 :
         play_sound("alert_500m")
         flag500 = True
-    elif !flag200 and shortest_distance < 200 and shortest_distance >100 :
+    elif (not flag200) and shortest_distance < 200 and shortest_distance >100 :
         play_sound("alert_200m")
         flag200 = True
-    elif !flag100 and shortest_distance < 100:
+    elif (not flag100) and shortest_distance < 100:
         play_sound("alert_100m")
         flag100 = True
